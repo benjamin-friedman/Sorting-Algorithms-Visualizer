d78fb4e35f5689291d78b8ebbef748ad7f65578e
benjamin-friedman <82235512+benjamin-friedman@users.noreply.github.com>
2021-11-09 15:33:10 -0500

    Add files via upload

---
+++ Program Files/SortingAlgorithms.h
@@ -0,0 +1,21 @@
+/*
+  SortingAlgorithms.h - The header file for all of the sorting algorithms
+*/
+
+
+#ifndef SORTING_ALGORITHMS_H
+#define SORTING_ALGORITHMS_H
+
+/* Sorting Algorithms */
+void bubbleSort(int* a, int size);
+void selectionSort(int* a, int size);
+void insertionSort(int* a, int size);
+void shellSort(int* a, int size);
+void quickSort(int* a, int size);
+void heapSort(int* a, int size);
+void mergeSortWrapper(int* a, int size);
+void countingSort(int* a, int size);
+void bucketSort(int* a, int size);
+void radixSort(int* a, int size);
+
+#endif
